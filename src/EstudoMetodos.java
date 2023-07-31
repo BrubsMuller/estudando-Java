17433d3523c63709b4503100cc2db5cabe6fb347
BrubsMuller <muller.vendeta@hotmail.com>
2023-07-31 17:16:43 -0300

    metodos

--- src/EstudoMetodos.java
+++ src/EstudoMetodos.java
@@ -1,29 +1,45 @@
-enum NivelAcesso {
-    COMUM, USUARIO, ADMINISTRADOR
-}
+public class EstudoMetodos {
+    class Garrafa {
+        float volume;
 
-class Pessoa {
-    String nome;
-    int idade;
-    NivelAcesso nivelAcesso;
-}
-
-public class EstudoEnums {
-
-    public static void main(String[] args) {
-        Pessoa davi = new Pessoa();
-        davi.nome = "Davi";
-        davi.idade = 8;
-        davi.nivelAcesso = NivelAcesso.USUARIO;
-
-        if(davi.nivelAcesso == NivelAcesso.ADMINISTRADOR) {
-            System.out.println("Libera tudo!");
+        public float obterVolume() {
+            return this.volume;
         }
-        else if(davi.nivelAcesso == NivelAcesso.USUARIO) {
-            System.out.println("Libera somente area privada!");
+    }
+
+    public class EstudoMetodos {
+        // Métodos/Funções -> São estruturas executam uma determinada terafa/atividade
+
+        // Funções Que Recebem Parâmetros e Quem Não Recebem
+        // Funções Que Retorna Valores e Quem Não Retornam
+        public String bemVindo(String nome) {
+            return "Bem vindo, " + nome;
         }
-        else if(davi.nivelAcesso == NivelAcesso.COMUM) {
-            System.out.println("Libera somente area publica!");
+
+        public void imprimir(String texto) {
+            System.out.println(texto);
+        }
+
+        public static double soma(double n1, double n2) {
+            double s = n1 + n2;
+            return s;
+        }
+
+        public static double soma(double n1, double n2, double n3) {
+            double s = n1 + n2 + n3;
+            return s;
+        }
+
+        public static void main(String[] args) {
+            double resultado = soma(12.1, 34.3, 7.4);
+            resultado = soma(12.1, 34.3);
+//        System.out.println(resultado);
+
+            Garrafa garrafa = new Garrafa();
+            garrafa.volume = 1.5F;
+
+            float volumeGarrafa = garrafa.obterVolume();
+            System.out.println(volumeGarrafa + " Litros");
         }
     }
 }
